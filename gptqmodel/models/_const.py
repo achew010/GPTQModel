c0711d517a0212eff4a3438ddfd65453f00de20f
1000850000 user <aaron.chew1@ibm.com>
2024-06-30 15:46:26 +0000

    const deletion

--- gptqmodel/models/_const.py
+++ gptqmodel/models/_const.py
@@ -19,40 +19,13 @@ CPU = device("cpu")
 CUDA_0 = device("cuda:0")
 
 SUPPORTED_MODELS = [
-    "bloom",
-    "gptj",
-    "gpt2",
     "gpt_neox",
-    "opt",
-    "moss",
     "gpt_bigcode",
-    "codegen",
-    "chatglm",
-    "RefinedWebModel",
-    "RefinedWeb",
-    "baichuan",
-    "internlm",
-    "qwen",
-    "xverse",
-    "deci",
-    "stablelm_epoch",
-    "mpt",
     "llama",
-    "longllama",
-    "falcon",
     "mistral",
-    "Yi",
     "mixtral",
-    "qwen2",
-    "phi",
-    "phi3",
     "gemma",
-    "starcoder2",
-    "cohere",
-    "minicpm",
-    "qwen2_moe",
     "dbrx_converted",
-    "deepseek_v2"
 ]
 
 EXLLAMA_DEFAULT_MAX_INPUT_LENGTH = 2048
